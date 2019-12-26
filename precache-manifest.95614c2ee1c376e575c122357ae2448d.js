f199ae56826b6fbbf3df2ef0c09d88bc55c36803
osbaldomartinez20 <omartin6@mail.sfsu.edu>
2019-12-25 21:43:18 -0800

    Updates

---
+++ precache-manifest.95614c2ee1c376e575c122357ae2448d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3cae925d61aa7c94ed887c06081ce57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c21032d70e8f649baf8a",
+    "url": "/static/css/main.a3581aa2.chunk.css"
+  },
+  {
+    "revision": "8b2997906d25abee3626",
+    "url": "/static/js/2.c3f9974e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.c3f9974e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c21032d70e8f649baf8a",
+    "url": "/static/js/main.f5a34ffc.chunk.js"
+  },
+  {
+    "revision": "565014f5507082636f62",
+    "url": "/static/js/runtime-main.a1095af6.js"
+  },
+  {
+    "revision": "db1d36476fef14f8306534b4beac03a5",
+    "url": "/static/media/download.db1d3647.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
